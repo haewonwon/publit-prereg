91e8b6e27da80f1841e4e5999f7f5f393ca1c7b4
haewonwon <khcw1029@daum.net>
2025-04-25 18:31:56 +0900

    Feat: components/ui 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">퍼블릿 사전등록 페이지</h1>
+    </main>
+  );
+}
